4a14177977620e81e6e1af3016833719926d5f4d
iAmTheBest4222 <keshumishra987@gmail.com>
2024-09-12 22:14:44 +0530

    add backend

--- application/controlers.py
+++ application/controlers.py
@@ -2,7 +2,7 @@ from flask import render_template,request,Flask
 from flask import current_app as app
 from application.models import *
 
-
+b=dict()
 
 
 @app.route("/",methods=["GET","POST"])
@@ -11,4 +11,13 @@ def f1():
         return render_template("index.html")
     else:
         a=dict(request.form)
-        return render_template("form.html")
+        for i in a:
+            b[i]=a[i]
+        if "login" in a:
+            
+            return render_template("form.html",a=b)
+        else:
+            if "submit" in a:
+                return render_template("dumy.html",a=b)
+            return render_template("form.html",a=b)
+
